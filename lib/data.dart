bca13153a4d49c5f7501c30f1d0696879cb84516
Muhammed Muneef M <md.muneefnk@gmail.com>
2023-08-22 00:47:23 +0530

    added list of items

---
+++ lib/data.dart
@@ -0,0 +1,17 @@
+class Data{
+  final String image;
+  final String name;
+
+  Data({required this.image, required this.name});
+}
+
+
+List<Data> data=[
+  Data(image: "assets/doctor.png", name: "Dr.John"),
+  Data(image: "assets/dr3.png", name: "Dr.David"),
+  Data(image: "assets/dr7.png", name: "Dr.Michael"),
+  Data(image: "assets/dr5.png", name: "Dr.James"),
+  Data(image: "assets/dr8.png", name: "Dr.Robert"),
+  Data(image: "assets/dr4.png", name: "Dr.William"),
+  Data(image: "assets/dr6.png", name: "Dr.Jennifer"),
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'data.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,74 +13,151 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
           backgroundColor: Colors.white,
-          body:
-          Center(
-            child: Container(
-              width: 350,
-              height: 180,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.grey[200]
-              ),
-              child: Row(
-                children: [
-                  Container(
-                    // color: Colors.red,
-                    padding: EdgeInsets.only(top: 10, left: 10, bottom: 2.4),
-                      child: Image.asset('assets/doctor.png', fit: BoxFit.cover,width: 130, height: 200,)),
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(20),
-                      // width: 200,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text('Dr.Pawan', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
-                              Icon(Icons.favorite_outline,color: Colors.blue,)
-                            ],
-                          ),
-                          SizedBox(height: 10,),
-                          Text('publishing and graphic design, Lorem ipsum is a placeholder text commonly', style: TextStyle(fontSize: 13, fontWeight: FontWeight.w500),),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                width: 80,
-                                height: 35,
-                                decoration: BoxDecoration(
-                                  color: Colors.blue,
-                                  borderRadius: BorderRadius.circular(20)
-                                ),
-                                child: Center(child: Text('Book', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 16),)),
-                              ),
-                              Row(
-                                children: [
-                                  Icon(Icons.star, color: Colors.orange,size: 25,),
-                                  Text('5.0', style: TextStyle(fontWeight: FontWeight.bold),),
-
-                                ],
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            leading: Icon(
+              CupertinoIcons.back,
+              color: Colors.black,
+            ),
+            title: Text(
+              "All Doctors",
+              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.cyan),
+            ),
+            centerTitle: true,
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  width: 350,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: Colors.grey[100],
+                      borderRadius: BorderRadius.circular(20)),
+                  child: ListTile(
+                    leading: Icon(Icons.search),
+                    title: TextField(
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Search a Doctor"),
                     ),
+                    trailing: Icon(Icons.mic),
                   ),
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                        width: 350,
+                        height: 180,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.grey[100]),
+                        child: Row(
+                          children: [
+                            Container(
+                                padding: EdgeInsets.only(
+                                    top: 10, left: 10),
+                                child: Image.asset(
+                                  data[index].image,
+                                  fit: BoxFit.fitHeight,
+                                  width: 130,
+                                  height: 180,
+                                )),
+                            Expanded(
+                              child: Container(
+                                padding: EdgeInsets.all(20),
+                                // width: 200,
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          data[index].name,
+                                          style: TextStyle(
+                                              fontSize: 22,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Icon(
+                                          Icons.favorite_outline,
+                                          color: Colors.cyan,
+                                        )
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      'publishing and graphic design, Lorem ipsum is a placeholder text commonly',
+                                      style: TextStyle(
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Container(
+                                          width: 80,
+                                          height: 35,
+                                          decoration: BoxDecoration(
+                                              color: Colors.cyan,
+                                              borderRadius:
+                                                  BorderRadius.circular(20)),
+                                          child: Center(
+                                              child: Text(
+                                            'Book',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.white,
+                                                fontSize: 16),
+                                          )),
+                                        ),
+                                        Row(
+                                          children: [
+                                            Icon(
+                                              Icons.star,
+                                              color: Colors.orange,
+                                              size: 25,
+                                            ),
+                                            Text(
+                                              '5.0',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    })
+              ],
             ),
           ),
-
-
-
-
-
         ));
   }
 }
